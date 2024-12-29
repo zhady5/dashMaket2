de145d6dbf863fdd18e3d061dd27e0612fa08cc1
Mika <42176090+zhady5@users.noreply.github.com>
2024-12-29 17:02:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -14,29 +14,6 @@ channels, posts, reactions, subscribers, views = load_data()
 processed_data = process_data(channels, posts, reactions, subscribers, views)
 
 
-import signal
-import sys
-
-class SignalHandler:
-    def __init__(self, signum, handler):
-        self.signum = signum
-        self.handler = handler
-        self.old_handler = None
-
-    def __enter__(self):
-        self.old_handler = signal.getsignal(self.signum)
-        signal.signal(self.signum, self.handler)
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        signal.signal(self.signum, self.old_handler)
-
-def sigterm_handler(signum, frame):
-    print("Received SIGTERM, shutting down server...")
-    sys.exit(0)
-
-signal.signal(signal.SIGTERM, sigterm_handler)
-
 # Настройка приложения Dash
 external_stylesheets = [
     'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css',
@@ -52,4 +29,4 @@ app.layout = create_layout(processed_data)
 register_callbacks(app, processed_data)
 
 if __name__ == '__main__':
-     app.run_server(debug=True)
+     app.run_server(debug=True, port=8000)
