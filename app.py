99e1c8da1f99fd71301e763d98695221b72979d2
Mika <42176090+zhady5@users.noreply.github.com>
2024-12-29 17:11:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -59,5 +59,5 @@ except Exception as e:
     ])
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    app.run_server(host='0.0.0.0', port=port, debug=False)
+    port = int(os.environ.get("PORT", 8888))
+    app.run_server( port=port, debug=False)
